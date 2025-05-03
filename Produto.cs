31e27aeb4799fde7c064c76ba374feda27da2b5c
Nathalia Monteiro <n.monteiro@unifesp.br>
2025-05-02 23:20:24 -0300

    Primeiro commit do Sistema de Estoque da Vinheria Agnello

---
+++ Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueVinheriaAgnello
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public string Safra { get; set; }
+        public string Pais { get; set; }
+        public decimal PrecoVenda { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, string tipo, string safra, string pais, decimal precoVenda, int quantidade)
+        {
+            Nome = nome; Tipo = tipo; Safra = safra; Pais = pais; PrecoVenda = precoVenda; Quantidade = quantidade;
+        }
+
+        public Produto()
+        {
+
+        }
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using EstoqueVinheriaAgnello;
+internal class Program
+{
+    static List<Produto> produtos = new List<Produto>();
+
+    static void Main(string[] args)
+    {
+        bool executando = true;
+
+        while (executando)
+        {
+            Console.WriteLine("\n **** BEM VINDO AO SISTEMA DE ESTOQUE DA VINHERIA AGNELLO ****");
+            Console.WriteLine("1. Cadastrar novo produto");
+            Console.WriteLine("2. Consultar estoque");
+            Console.WriteLine("3. Registrar entrada de estoque");
+            Console.WriteLine("4. Registrar saída de estoque");
+            Console.WriteLine("0. Sair");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    CadastrarProduto();
+                    break
+                case "2":
+                    ConsultarEstoque();
+                    break
+                case "3":
+                    RegistrarEntrada();
+                    break;
+                case "4":
+                    RegistrarSaida();
+                    break;
+                case "0":
+                    executando = false;
+                    Console.WriteLine("Finalizando...");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente:");
+                    break;
+
+            }
+
+
+
+
+
+        }
+
+    }
+
+
+    static void CadastrarProduto()
+    {
+
+    }
+
+    static void ConsultarEstoque()
+    {
+
+    }
+
+    static void RegistrarEntrada()
+    {
+
+    }
+
+    static void RegistrarSaida()
+    {
+
+    }
+}
